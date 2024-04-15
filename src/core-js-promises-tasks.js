83495e3f42a1a8434e61cc13630b4613ad60ac15
Pavel Gordienko <guzmo@yandex.ru>
2024-04-16 02:55:23 +0300

    feat: add solution for queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -149,8 +149,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce(async (process, promise) => {
+    const result = await process;
+    const value = await promise;
+    return result + value;
+  }, Promise.resolve(''));
 }
 
 module.exports = {
